eeb0c68a92814b013b98bf9a633787d23d786f69
ttdragon <a0909956502@gmail.com>
2023-03-08 17:11:37 +0800

    update

--- js/anim.js
+++ js/anim.js
@@ -9,31 +9,52 @@ $(document).ready(function () {
 
 
 $(window).on('load', function () {
+    TL.set(
+        '.main_subtitle',
+        {
+            y:'-30%'
+        }
+    ).set(
+        '.main_wrap_group',
+        {
+            y:'-30%'
+        }
+    ).set(
+        '.keep_read_btn',
+        {
+            y:'-30%'
+        }
+    )
+
     TL.to(
         '.main_title',
         {
             opacity: 1,
-            duration: 1
+            duration: 1,
+            y:0
         }
     ).to(
         '.main_subtitle',
         {
             opacity: 1,
-            duration: 1
+            duration: 1,
+            y:0
         },
         '>'
     ).to(
         '.main_wrap_group',
         {
             opacity: 1,
-            duration: 1
+            duration: 1,
+            y:0
         },
         '<'
     ).to(
         '.keep_read_btn',
         {
             opacity: 1,
-            duration: 1
+            duration: 1,
+            y:0
         },
         '<'
     );
@@ -47,7 +68,7 @@ $(window).on('load', function () {
                 start:'top center'
             },
             opacity:1,
-            duration:1
+            duration:0.5
         }
     );
     gsap.to(
